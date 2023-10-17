5e0d39f4774c080dd95580fbe0b8a82bdde04143
Viktor Mamonov <mamonov.victor@gmail.com>
2023-10-17 20:57:56 +0200

    Test commit

--- time-optimizer-app/src/main/java/dev/mamonov/timeoptimizer/App.java
+++ time-optimizer-app/src/main/java/dev/mamonov/timeoptimizer/App.java
@@ -1,7 +1,14 @@
 package dev.mamonov.timeoptimizer;
 
 public class App {
+	public int ii;
 	public static void main(String[] args) {
-		System.out.println(8888);
+		try {
+			System.out.println("FJLDKJF");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
