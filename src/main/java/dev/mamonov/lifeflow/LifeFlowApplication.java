c193178de685417a1289cdabf247f6a3c042ccc4
Viktor Mamonov <mamonov.victor@gmail.com>
2024-01-14 16:26:05 +0100

    Implementation of Spring Boot

---
+++ src/main/java/dev/mamonov/lifeflow/LifeFlowApplication.java
@@ -0,0 +1,13 @@
+package dev.mamonov.lifeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mamonov/lifeflow/LifeFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mamonov.lifeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
